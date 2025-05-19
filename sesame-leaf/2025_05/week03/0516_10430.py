12390919f2c318d8b5709b6c918680de7926d3d1
sesame-leaf <bangjunseok2005@naver.com>
2025-05-19 15:45:50 +0900

    feat: Solve 10430

---
+++ sesame-leaf/2025_05/week03/0516_10430.py
@@ -0,0 +1,12 @@
+def main():
+    A, B, C = map(int, input().split())
+    
+    print((A+B)%C)
+    print(((A%C)+(B%C))%C)
+    print((A*B)%C)
+    print(((A%C)*(B%C))%C)
+
+
+if __name__ == "__main__":
+    main()
+    
